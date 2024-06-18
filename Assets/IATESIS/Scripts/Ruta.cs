3e1aae5c981d221511f307db7b922d814387bc06
C.Ruiz <74808642+cruiz311@users.noreply.github.com>
2024-06-18 02:07:34 -0500

    IntentoFallido

--- Assets/IATESIS/Scripts/Ruta.cs
+++ Assets/IATESIS/Scripts/Ruta.cs
@@ -6,10 +6,20 @@ public class Ruta : MonoBehaviour
 {
     public Transform initPosition;
     public GameObject carro;
-
+    public List<GameObject> alineaciones;
     private void Start()
     {
         GameObject nuevoCarro = Instantiate(carro, initPosition.position, carro.transform.rotation);
+        Init();
+    }
 
+
+    public void Init()
+    {
+        foreach (GameObject t in alineaciones)
+        {
+            t.SetActive(false);
+            alineaciones[0].SetActive(true);
+        }
     }
 }
